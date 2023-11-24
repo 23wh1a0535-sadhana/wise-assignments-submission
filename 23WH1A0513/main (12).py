1cf23d562e6f9464e7ce9447803541dc3de76024
23wh1a0513 <150132045+23wh1a0513@users.noreply.github.com>
2023-11-24 05:54:32 +0530

    Add files via upload

---
+++ 23WH1A0513/main (12).py
@@ -0,0 +1,5 @@
+n = int(input("Enter the value of n: "))   
+num = n                                 
+while num >= 1:                           
+    print(num)                             
+    num -= 1                        
