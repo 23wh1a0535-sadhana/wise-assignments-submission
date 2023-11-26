22ea7ac011e2b56fa4e579b9088e76dd024b96e7
23wh1a0526 <150162223+23wh1a0526@users.noreply.github.com>
2023-11-26 12:47:32 +0530

    Create vertical_menu.py

---
+++ 23WH1A0526/RESTUARANT MENU/vertical_menu.py
@@ -0,0 +1,6 @@
+#print list of menu then each element out of the list in vertical line.
+def menu():
+    menu = ["Biryani","Fried Rice","Bagara","Schezwaan Rice","Plane Rice","Curd Rice","Vangi Baat"]
+    for x in menu:
+        print(x)
+menu()
