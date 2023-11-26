6de80d73b6f3bdcd750c099bb4b192972847ac6d
23wh1a0519 <150130821+23wh1a0519@users.noreply.github.com>
2023-11-26 20:37:04 +0530

    Create functions8.py

---
+++ 23-19/functions8.py
@@ -0,0 +1,7 @@
+def state():
+    city=input("Enter a city ")
+    if city=="hyderabad" or city=="siddipet" or city=="warangal":
+        print("city is in telangana")
+    else:
+        print("city is not in telangana")
+state()
