10f336e446a9596e87cb44bcd822da9f75d331ec
23wh1a0519 <150130821+23wh1a0519@users.noreply.github.com>
2023-11-26 20:43:46 +0530

    Create functions10.py

---
+++ 23-19/functions10.py
@@ -0,0 +1,9 @@
+def reverse():
+    num=int(input("Enter a number: "))
+    reverse = 0
+    while num != 0:
+        digit = num % 10
+        reverse= reverse * 10 + digit
+        num //= 10
+    print(reverse)
+reverse()
