e4f858c6e594b27683d61456ae13330fded7ecc3
23wh1a0519 <150130821+23wh1a0519@users.noreply.github.com>
2023-11-26 19:06:09 +0530

    Create loops8.py

---
+++ 23-19/loops8.py
@@ -0,0 +1,5 @@
+alphabet=input("Enter an alphabet")
+if alphabet=='a' or alphabet=='e' or alphabet=='i' or alphabet=='o' or alphabet=='u'or alphabet=='A' or alphabet=='E' or alphabet=='I' or alphabet=='O' or alphabet=='U':
+    print("vowel")
+else:
+    print("consonant")
