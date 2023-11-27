338d6f3eecd195efcc01a61d672769752aab36e9
23wh1a0535-sadhana <150142098+23wh1a0535-sadhana@users.noreply.github.com>
2023-11-27 16:41:02 +0530

    Create num of alpha and num q4.py

---
+++ 23WH1A0535/loops/num of alpha and num q4.py
@@ -0,0 +1,10 @@
+string = input("Enter a string: ")
+length = len(string)
+countnum = 0
+countalpha = 0
+for i in range(length):
+    if string[i].isnumeric():
+        countnum += 1
+    elif string[i].isalpha():
+        countalpha += 1
+print("The given string," , string , "has" , countalpha , "letters and" , countnum , "numbers.")
