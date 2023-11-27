13bc7b6e44b40dfee568ef502a740580a6ea13d5
23wh1a0535-sadhana <150142098+23wh1a0535-sadhana@users.noreply.github.com>
2023-11-27 18:10:18 +0530

    Create city in telangana q8.py

---
+++ 23WH1A0535/functions/city in telangana q8.py
@@ -0,0 +1,7 @@
+def state():
+    city = str(input("Enter a city ")).lower()
+    if city =="hyderabad" or city =="medchal" or city =="warangal":
+        print("city is in telangana")
+    else:
+        print("city is not in telangana")
+state()
