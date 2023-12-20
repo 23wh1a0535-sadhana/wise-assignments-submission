d0de9a847caf5e0f8b1c15b80fad54f6d616de5c
23wh1a0535-sadhana <150142098+23wh1a0535-sadhana@users.noreply.github.com>
2023-12-20 23:03:53 +0530

    problem5.py

--- 23WH1A0535/higer order functions/problem5.py
+++ 23WH1A0535/higer order functions/problem5.py
@@ -1,3 +1,5 @@
+#5. Write a python program to create a list of names of students, their grades and join them together in the form of tuples.
+
 names = ["mahesh", "sukesh", "mukesh"]
 roll_no = [112 ,113, 114]
 marks = [78, 34, 90]
