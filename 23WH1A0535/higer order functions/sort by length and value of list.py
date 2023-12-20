363451e4af4d5d71b40aa06c5ee89028d24ba7bb
23wh1a0535-sadhana <150142098+23wh1a0535-sadhana@users.noreply.github.com>
2023-12-20 22:55:27 +0530

    sort by length and value of list.py

--- 23WH1A0535/higer order functions/sort by length and value of list.py
+++ 23WH1A0535/higer order functions/sort by length and value of list.py
@@ -1,3 +1,5 @@
+#2. Write a Python program to sort a given list of lists by length and value using lambda.
+
 list_of_lists = [[1, 2, 3], [4, 5], [6, 7, 8, 9], [10]]
 sorted_list = sorted(list_of_lists, key=lambda x: (len(x), x))
 print(sorted_list)
