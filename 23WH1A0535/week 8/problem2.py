b14a712ca5d43e6391a5584486f30e6b5d99d49a
23wh1a0535-sadhana <150142098+23wh1a0535-sadhana@users.noreply.github.com>
2023-12-20 20:15:18 +0530

    problem2.py

--- 23WH1A0535/week 8/problem2.py
+++ 23WH1A0535/week 8/problem2.py
@@ -1,3 +1,17 @@
+#Problem 2
+#Take any four-digit number, using at least two different digits (leading zeros are allowed).
+#Arrange the digits in descending and then in ascending order to get two four-digit numbers,adding leading zeros if necessary.
+#Subtract the smaller number from the bigger number.
+#Go back to step 2 and repeat. This process stops after a few steps, that is step 3 produces the same number. 
+#DO this with paper and pen to understand the same Start with 1945
+#9541–1459=8082
+#8820–0288=8532
+#8532–2358=6174
+#7641–1467=6174
+#Given a suitable starting number generate the sequence. It should not repeat; that is it should stop before the repetition.
+#For example if 1945 is input it should return [1945,8082,8532,6174]
+
+
 def sequence(starting_number):
     seq = [starting_number]
 
@@ -15,6 +29,5 @@ def sequence(starting_number):
 
     return seq
 
-# Example usage with starting number 1945
 result_sequence = sequence(1945)
 print(result_sequence)
