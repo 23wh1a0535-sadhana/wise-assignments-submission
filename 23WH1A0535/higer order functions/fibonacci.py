c55fc587ceeaa03b51f73d026dafbcca349c6349
23wh1a0535-sadhana <150142098+23wh1a0535-sadhana@users.noreply.github.com>
2023-12-20 22:54:09 +0530

    fibonacci.py

--- 23WH1A0535/higer order functions/fibonacci.py
+++ 23WH1A0535/higer order functions/fibonacci.py
@@ -1,3 +1,5 @@
+#1. Write a Python program to create Fibonacci series up to n using Lambda.
+
 fibonacci = lambda n: [0, 1] if n == 2 else fibonacci(n-1) + [fibonacci(n-1)[-1] + fibonacci(n-1)[-2]]
 
 n = int(input("Enter the value of n: "))
